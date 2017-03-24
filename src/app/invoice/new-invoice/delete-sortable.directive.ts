2b69703b33656a1b3f32ad46f9ca28813fd73f82
sandipyuvitime <sandip@yuvitime.com>
2017-03-24 20:16:16 +0530

    css fixings

--- src/app/invoice/new-invoice/delete-sortable.directive.ts
+++ src/app/invoice/new-invoice/delete-sortable.directive.ts
@@ -12,6 +12,30 @@ export class DeleteSortableDirective implements OnInit{
     ngOnInit(){
         jQuery(this.elementRef.nativeElement).on('click', function(){
             jQuery(this).parent().parent().parent().remove();
+            var totalCost: any = 0;
+            jQuery('.invoice-items').find('input[name=item-cost]').each(function(){
+                var rowCost = jQuery(this).val();
+                if(rowCost == ""){
+                    rowCost = 0;
+                }
+                totalCost = parseFloat(totalCost) + parseFloat(rowCost);
+            });
+            totalCost = totalCost.toFixed(2);
+            jQuery('#cost-subtotal').html("$"+totalCost);
+            var discountOption = jQuery("#discount-option").val();
+            var discountVal = jQuery('#discount-value').val();
+            if(discountVal == ""){
+                discountVal = 0;
+            }
+            if(discountOption == "percent"){
+                totalCost = totalCost - ((discountVal / 100) * totalCost);
+            }
+            else{
+                totalCost = totalCost - discountVal;
+            }
+            //console.log(totalCost);
+            totalCost = totalCost.toFixed(2);
+            jQuery('#item-net-total').html("$"+totalCost);
         });
     }
 
