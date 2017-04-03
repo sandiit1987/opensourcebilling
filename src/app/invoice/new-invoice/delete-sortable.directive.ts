8d74ac763a9445781bc1a4eff845c5eae7da1522
sandipyuvitime <sandip@yuvitime.com>
2017-04-03 20:03:05 +0530

    discount percentage on row  delete

--- src/app/invoice/new-invoice/delete-sortable.directive.ts
+++ src/app/invoice/new-invoice/delete-sortable.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, ElementRef, OnInit } from '@angular/core';
+import { CalculateService } from './calculate.service';
 declare var jQuery: any;
 @Directive({
   selector: '[deleteSortable]'
@@ -6,7 +7,7 @@ declare var jQuery: any;
 export class DeleteSortableDirective implements OnInit{
 
     private elementRef: ElementRef;
-    constructor(elementRef: ElementRef) {
+    constructor(elementRef: ElementRef, private calculateService: CalculateService) {
         this.elementRef = elementRef;
         jQuery(this.elementRef.nativeElement).on('click', function(){
             jQuery(this).parent().parent().parent().remove();
@@ -20,7 +21,41 @@ export class DeleteSortableDirective implements OnInit{
             });
             totalCost = totalCost.toFixed(2);
             jQuery("#item-net-total").html("$"+totalCost);
+
+            /* Total discount calculation */
+            var totalDiscount: any = 0;
+            jQuery('.invoice-items').find('input[name=item-discount]').each(function(){
+                var rowDiscount = jQuery(this).val();
+                if(rowDiscount == ""){
+                    rowDiscount = 0;
+                }
+                totalDiscount = parseFloat(totalDiscount) + parseFloat(rowDiscount);
+            });
+            totalDiscount = totalDiscount.toFixed(2);
+            console.log("totalDiscount");
+            console.log(totalDiscount);
+            if(isNaN(totalDiscount)){
+                totalDiscount = 0;
+            }
+            jQuery("#discount-value").val(totalDiscount);
+            /* Total tax calculation */
+            var totalTax: any = 0;
+            jQuery('.invoice-items').find('select[name=tax-id]').each(function(){
+
+                var rowTaxId = jQuery(this).val();
+                var rowItemUnitCost = jQuery(this).closest('tr').find('input[name=item-unit-cost]').val();
+                var rowItemQty = jQuery(this).closest('tr').find('input[name=item-qty]').val();
+                var rowItemDiscount = jQuery(this).closest('tr').find('input[name=item-discount]').val();
+                //console.log(rowItemUnitCost+" "+rowItemQty+" "+rowItemDiscount);
+                var rowTax: any = calculateService.getTaxAmount(rowItemUnitCost, rowItemQty, rowItemDiscount, rowTaxId);
+                totalTax = parseFloat(totalTax) + parseFloat(rowTax);
+            });
+            if(isNaN(totalTax)){
+                totalTax = 0;
+            }
+            jQuery('#tax-value').val(totalTax);
         });
+
     }
     ngOnInit(){
 
